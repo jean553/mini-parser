ca49d8b9284b6b0ad2bda3990cdb2619e9e2f768
jean <jean@bst>
2017-01-22 00:22:21 +0800

    replace recursive function by for loop

--- src/main.rs
+++ src/main.rs
@@ -5,102 +5,31 @@ use std::process;
 use std::io::stderr;
 use std::io::Write;
 
-mod lexical_analyzer;
+#[derive(PartialEq)]
+enum Token {
+    Digit,
+    Plus,
+    Unknown
+}
 
-/// Generates assembly instructions when a digit is met
+/// Lexical analyze of one character and return a token for parsing.
 ///
 /// # Arguments:
 ///
-/// * `ouput` - String object to generate the ouput assembly
-/// (mutable reference in order to add code)
 /// * `character` - character to check
-fn read_digit(
-    output: &mut std::string::String,
-    character: char
-) {
-    output.push_str("mov eax, ");
-    output.push(character);
-    output.push('\n');
-}
+fn get_token_from_character(character: char) -> Token {
 
-/// Generates assembly instructions when a '+' is met
-///
-/// # Arguments:
-///
-/// * `iterator` - iterator over the input characters
-/// (mutable reference in order to iterate)
-/// * `ouput` - String object to generate the ouput assembly
-/// (mutable reference in order to add code)
-fn read_plus(
-    iterator: &mut std::str::Chars,
-    output: &mut std::string::String
-) {
-    output.push_str("mov ebx, eax\n");
-
-    let character = iterator.next();
     match character {
-        Some(character) => {
-            read_digit(output, character);
+        '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => {
+            return Token::Digit;
         }
-        None => {}
-    }
-
-    output.push_str("add eax, ebx\n");
-}
-
-/// Check if the given character matches with a symbol type
-///
-/// # Arguments:
-///
-/// * `iterator` - iterator over the input characters
-/// (mutable reference in order to iterate)
-/// * `ouput` - String object to generate the ouput assembly
-/// (mutable reference in order to add code)
-/// * `character` - character to check
-fn handle_character(
-    iterator: &mut std::str::Chars,
-    output: &mut std::string::String,
-    character: char
-)
-{
-    let character_is_digit: bool = lexical_analyzer::is_digit(character);
-    let character_is_plus: bool = lexical_analyzer::is_plus(character);
-
-    if !character_is_digit && !character_is_plus {
-        return;
-    }
-
-    if character_is_digit {
-        read_digit(output, character);
-    }
-
-    if character_is_plus {
-        read_plus(iterator, output);
-    }
-
-    iterate(iterator, output);
-}
-
-/// Recursive method that reads the given instructions from 'iterator'
-/// and fills the 'output' string object with assembly code
-///
-/// # Arguments
-///
-/// * `iterator` - iterator over the input characters
-/// (mutable reference in order to iterate)
-/// * `ouput` - String object to generate the ouput assembly
-/// (mutable reference in order to add code)
-fn iterate(
-    iterator: &mut std::str::Chars,
-    output: &mut std::string::String
-) {
-    let character = iterator.next();
-    match character {
-        Some(character) => {
-            handle_character(iterator, output, character);
+        '+' => {
+            return Token::Plus;
         }
-        None => {}
-    };
+        _ => {
+            return Token::Unknown;
+        }
+    }
 }
 
 fn main() {
@@ -123,8 +52,31 @@ _start:
 "#
     );
 
-    let mut iterator = args[1].chars();
-    iterate(&mut iterator, &mut output);
+    let mut previous = Vec::new();
+
+    for (index, character) in args[1].chars().enumerate() {
+        let token: Token = get_token_from_character(character);
+
+        if token == Token::Digit {
+            output.push_str("mov eax, ");
+            output.push(character);
+            output.push('\n');
+
+            // TODO: won't work with numbers, must be improved
+            if index != 0 {
+                let prev: char = previous[index - 1];
+                if get_token_from_character(prev) == Token::Plus {
+                    output.push_str("add eax, ebx\n");
+                }
+            }
+        }
+
+        if token == Token::Plus {
+            output.push_str("mov ebx, eax\n");
+        }
+
+        previous.push(character);
+    }
 
     output +=
 r#"
